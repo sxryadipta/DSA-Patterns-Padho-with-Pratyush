f58a0afa743db1ff6fac91b6a2d9da7f01f7299e
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-06-09 23:51:26 +0530

    Fix time complexity comment in ReverseIntegers.cpp
    Updated time complexity comment for clarity.

--- Basic Maths/ReverseIntegers.cpp
+++ Basic Maths/ReverseIntegers.cpp
@@ -1,5 +1,28 @@
 // Problem  : Reverse Integers
 // Leetcode : https://leetcode.com/problems/reverse-integer/description/
 // Pattern  : Basic Maths
-// TC       : O(log₁₀n)
+// TC       : O(log₁₀x)
 // SC       : O(1)
+class Solution {
+public:
+    int reverse(int x) {
+        int number = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+
+            if (number > INT_MAX / 10 ||
+                (number == INT_MAX / 10 && lastDigit > 7))
+                return 0;
+
+            if (number < INT_MIN / 10 ||
+                (number == INT_MIN / 10 && lastDigit < -8))
+                return 0;
+
+            number = number * 10 + lastDigit;
+            x /= 10;
+        }
+
+        return number;
+    }
+};
