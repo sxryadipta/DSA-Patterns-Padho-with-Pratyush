a16c4986b047fd13300caf83bf490eb3929119c8
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-06-09 23:04:55 +0530

    Create ReverseDigits.cpp with initial class structure
    Added a class Solution with a method to reverse digits.

---
+++ Basic Maths/ReverseDigits.cpp
@@ -0,0 +1,12 @@
+// Problem  : Reverse Digits
+// GeeksforGeeks : https://www.geeksforgeeks.org/problems/reverse-digit0316/1
+// Pattern  : Basic Maths
+// TC       : O(n)
+// SC       : O(n)
+class Solution {
+  public:
+    int reverseDigits(int n) {
+        // Code here
+        
+    }
+};
