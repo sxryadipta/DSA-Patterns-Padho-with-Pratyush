cb6e921226904ea3dbb374c9366cd0fd3346ae84
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-06-09 22:12:09 +0530

    Refactor countDigits function to simplify logic
    Remove unnecessary variable 'lastDigit' from countDigits function.

--- Basic Maths/01-Count-digits.cpp
+++ Basic Maths/01-Count-digits.cpp
@@ -1,10 +1,14 @@
+// Problem  : Count Digits in a number
+// GeeksforGeeks : https://www.geeksforgeeks.org/problems/count-total-digits-in-a-number/1
+// Pattern  : Basic Maths
+// TC       : O(logn)
+// SC       : O(1)
 class Solution {
   public:
     int countDigits(int n) {
         // Code here
         int count=0;
         while(n > 0){
-            int lastDigit = n % 10;
             count++;
             n= n/10;
         }
