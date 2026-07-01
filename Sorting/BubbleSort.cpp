bc42001a37af036fcb24cdd6e21aba71050a8d03
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-07-01 12:29:30 +0530

    Add bubble sort implementation
    Implemented the bubble sort algorithm to sort an array in ascending order.

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -3,3 +3,21 @@
 // Pattern  : Selection Sort
 // TC       : O(n²)
 // SC       : O(1)
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+      
+        // If no two elements were swapped, then break
+        if (!swapped)
+            break;
+    }
+}
