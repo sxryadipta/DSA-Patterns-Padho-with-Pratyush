753b15491e2fe61062199669bbe06de4d5389ea5
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-07-11 22:54:39 +0530

    Optimize sortColors using counting sort
    Replaced sorting with a counting sort approach for better performance.

--- 02-Two-Pointers/sortColors.cpp
+++ 02-Two-Pointers/sortColors.cpp
@@ -5,3 +5,32 @@ public:
         sort(nums.begin(), nums.end());
     }
 };
+
+//better
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        int zero = 0, one = 0, two = 0;
+
+        for (int num : nums) {
+            if (num == 0)
+                zero++;
+            else if (num == 1)
+                one++;
+            else
+                two++;
+        }
+
+        int i = 0;
+
+        while (zero--)
+            nums[i++] = 0;
+
+        while (one--)
+            nums[i++] = 1;
+
+        while (two--)
+            nums[i++] = 2;
+    }
+};
