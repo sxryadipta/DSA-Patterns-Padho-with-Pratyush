e04479510c3c052d6d90da58f80ef1a8ce1ca2e5
Suryadipta Ghosh <sxryadipta@gmail.com>
2026-08-01 12:37:54 +0530

    Add brute force method to check for duplicates
    Implement brute force solution to check for duplicates in an array.

---
+++ 01-Arrays-&-Hashing/containsDuplicate.cpp
@@ -0,0 +1,18 @@
+
+// Brute Force
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i < n; i++){
+          for(int j=i+1; j<n; j++){
+            if(nums[i] == nums[j]){
+                return true;
+            }
+          }  
+        }
+        return false;
+    }
+    
+
+};
